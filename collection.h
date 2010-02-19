cb132df72440bdaab9204ef5e2fee9675e74e2ed
Brian K <bri.kor.21@gmail.com>
2010-02-19 14:58:35 -0600

    Initial implementation of collection class just subclassing qtableview for now to get something to display, lots more work to come

---
+++ collection.h
@@ -0,0 +1,38 @@
+/*
+    Copyright (C) <2010>  Brian Korbein <bri.kor.21@gmail.com>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+*/
+
+
+#ifndef COLLECTION_H
+#define COLLECTION_H
+
+#include <QTableView>
+#include "collectiondb.h"
+
+class Collection : public QTableView
+{
+  Q_OBJECT
+  public:
+    Collection(QWidget* parent = 0);
+    
+  private:
+    CollectionDB *m_db;
+
+};
+
+#endif // COLLECTION_H
