9d582bcc5bd548ca8266c33fcbfd99e38c9464a8
Brian K <bri.kor.21@gmail.com>
2010-02-18 21:39:57 -0600

    Initial commit Mostly just setting the layout for initial work, collectiondb.h has an initial implementation but nothing tested or decided 100%

---
+++ collectiondb.h
@@ -0,0 +1,52 @@
+/*
+    Copyright (C) <2010>  Brian Korbein <bri.kor.21@gmail.com>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+*/
+
+
+#ifndef COLLECTIONDB_H
+#define COLLECTIONDB_H
+//KDE includes
+#include <KUrl>
+
+//QT includes
+#include <QSqlDatabase>
+#include <QSqlTableModel>
+#include <QString>
+
+class CollectionDB : public QObject
+{
+  Q_OBJECT
+  public:
+    CollectionDB();
+    ~CollectionDB();
+  
+  public signals:
+    void modelUpdate(QSqlTableModel *model);
+    
+  public slots:
+    void addBook(QString title, QString summary, QString author, QString release,
+	       QString releaseDate, KUrl *url);
+    void remBook(int row);
+    
+  private:
+    QSqlDatabase *db;
+    QSqlTableModel *model;
+    
+};
+
+#endif // COLLECTIONDB_H
