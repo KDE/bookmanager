890f05d4ef056836042140a7dc0185df0e627612
Brian K <bri.kor.21@gmail.com>
2011-06-27 10:20:15 -0500

    fix the last of the wordwrap errors, script is actually working now

--- tools/parse_valgrind_suppressions.sh
+++ tools/parse_valgrind_suppressions.sh
@@ -19,14 +19,12 @@ BEGIN { suppression=0; md5sum = "md5sum" }
   /^}/  {
           if (suppression)
            { suppression=0;
-             close(md5sum, "to")  # We've finished sending data to md5sum, so close that part of the 
-pipe
+             close(md5sum, "to")  # We've finished sending data to md5sum, so close that part of the pipe
              ProcessInput()       # Do the slightly-complicated stuff in functions
              delete supparray     # We don't want subsequent suppressions to append to it!
            }
      }
-  # Otherwise, it's a normal line. If we're inside a supression, store it, and pipe it to md5sum. 
-Otherwise it's cruft, so ignore it
+  # Otherwise, it's a normal line. If we're inside a supression, store it, and pipe it to md5sum. Otherwise it's cruft, so ignore it
      { if (suppression)
          { 
             supparray[++i] = $0
@@ -40,13 +38,11 @@ Otherwise it's cruft, so ignore it
     # Pipe the result from md5sum, then close it     
     md5sum |& getline result
     close(md5sum)
-    # gawk can't cope with enormous ints like $result would be, so stringify it first by prefixing a 
-definite string
+    # gawk can't cope with enormous ints like $result would be, so stringify it first by prefixing a definite string
     resultstring = "prefix"result
  
     if (! (resultstring in chksum_array) )
-      { chksum_array[resultstring] = 0;  # This checksum hasn't been seen before, so add it to the 
-array
+      { chksum_array[resultstring] = 0;  # This checksum hasn't been seen before, so add it to the array
         OutputSuppression()              # and output the contents of the suppression
       }
  }
