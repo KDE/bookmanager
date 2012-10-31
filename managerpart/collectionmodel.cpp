639967a8f6fa37347f99a2b805fdf77f7f56f1ba
Riccardo Bellini <ricky88ykcir@gmail.com>
2012-10-31 16:22:19 +0100

    Added copyright notices
    Added file COPYING-CMAKE-MODULES in cmake/modules to specify the
    copyright owner for FindPoppler.cmake
    Added copyright notice in other files of the project

--- managerpart/collectionmodel.cpp
+++ managerpart/collectionmodel.cpp
@@ -1,5 +1,6 @@
 /*
     Copyright (C) <2010>  Brian Korbein <bri.kor.21@gmail.com>
+    Copyright (C) 2011  Riccardo Bellini <ricky88ykcir@gmail.com>
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
