3beba9ec1e0bc3e6a371a6de48f3408e1ab165fa
Brian Korbein <bri.kor.21@gmail.com>
2013-08-11 19:21:20 -0500

    add a couple actual tests (it even passes) for the constructor of collectiondb. The CTest integration is broken, I think because it insists on popping up a migration popup every time it runs. Refactoring all the GUI components out of collectionDB will have to be done before it can really be automatic.

--- tests/collectiondbtests.cpp
+++ tests/collectiondbtests.cpp
@@ -1,30 +1,31 @@
 /*
- * Unit Tests for managerpart/Collectiondb
- * Copyright 2013  Brian k. <Bri.kor.21@gmail.com>
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License or (at your option) version 3 or any later version
- * accepted by the membership of KDE e.V. (or its successor approved
- * by the membership of KDE e.V.), which shall act as a proxy
- * defined in Section 14 of version 3 of the license.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
+ Unit Tests for managerpart/Collectiondb
+ Copyright (C) <2013>  Brian Korbein <bri.kor.21@gmail.com>
+ 
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ 
  */
 
 #include "collectiondbtests.h"
+#include "../managerpart/collectiondb.h"
 
+#include <QSqlDatabase>
+#include <QSqlQuery>
 #include <qtest_kde.h>
 
-QTEST_KDEMAIN_CORE(CollectiondbTests);
+QTEST_KDEMAIN(CollectiondbTests, GUI);
 
 void CollectiondbTests::initTestCase()
 {
@@ -48,7 +49,17 @@ void CollectiondbTests::cleanup()
 
 void CollectiondbTests::Construction()
 {
-
+    //all we really have testable in the constructor right now is the 
+    //connection. Migration and the failure modes really need to get 
+    //pulled out of the constructor so they can be tested...
+    //TODO
+    
+    m_db = new CollectionDB();
+    QStringList connectionNames = QSqlDatabase::connectionNames ();
+    //there should only be one connection
+    QCOMPARE(connectionNames.length(), 1);
+    //and it should be the default
+    QCOMPARE(connectionNames.first(), QString("qt_sql_default_connection"));
 }
 
 void CollectiondbTests::addbook()
--- tests/collectiondbtests.h
+++ tests/collectiondbtests.h
@@ -1,30 +1,28 @@
 /*
- * Unit Tests for managerpart/Collectiondb
- * Copyright 2013  Brian k. <Bri.kor.21@gmail.com>
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License or (at your option) version 3 or any later version
- * accepted by the membership of KDE e.V. (or its successor approved
- * by the membership of KDE e.V.), which shall act as a proxy
- * defined in Section 14 of version 3 of the license.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
+ Unit Tests for managerpart/Collectiondb
+ Copyright (C) <2013>  Brian Korbein <bri.kor.21@gmail.com>
+ 
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ 
  */
-
 #ifndef COLLECTIONDBTESTS_H
 #define COLLECTIONDBTESTS_H
 
 #include <QObject>
 
+class CollectionDB;
 class CollectiondbTests : public QObject
 {
     Q_OBJECT
@@ -39,6 +37,9 @@ private slots:
     void addbook();
     void isDirty();
     void checkDupe();
+    
+private:
+    CollectionDB *m_db;
 };
 
 #endif // COLLECTIONDBTESTS_H
