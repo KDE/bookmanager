ee0ac0ff1c80acf585904c7c6e019fc131883ae3
Riccardo Bellini <riccardo.bellini1988@gmail.com>
2014-01-02 22:44:59 +0100

    Add collection organizer widget
    Add a widget to retrieve the configuration options to be used in the
    organization of the collection.

---
+++ managerpart/collectionorganizerwidget.h
@@ -0,0 +1,38 @@
+/*
+    Copyright (C) 2014  Riccardo Bellini <riccardo.bellini1988@gmail.com>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#ifndef COLLECTION_ORGANIZER_H
+#define COLLECTION_ORGANIZER_H
+
+// Qt includes
+#include <qwidget.h>
+
+// Uis includes
+#include "ui_collectionorganizerwidget.h"
+
+
+class CollectionOrganizerWidget : public QWidget, private Ui::CollectionOrganizerWidget {
+public:
+    CollectionOrganizerWidget (QWidget * parent = 0, Qt::WindowFlags flags = 0);
+    virtual ~CollectionOrganizerWidget ();
+
+private:
+    /* data */
+};
+
+#endif // COLLECTION_ORGANIZER_H
