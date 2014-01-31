376d5c9492cb826bfa99b217ffced49e3b3c9920
Riccardo Bellini <riccardo.bellini1988@gmail.com>
2014-01-31 16:18:35 +0100

    Add header file of class StructureTokenizer

---
+++ managerpart/tools/collectionorganizer/structuretokenizer.h
@@ -0,0 +1,69 @@
+/*
+    Copyright (C) 2014  Riccardo Bellini <riccardo.bellini1988@gmail.com>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#ifndef STRUCTURE_TOKENIZER_H
+#define STRUCTURE_TOKENIZER_H
+
+// Book Manager includes
+#include "token.h"
+
+// Qt includes
+#include <qobject.h>
+
+
+namespace tokenizer
+{
+    /**
+     * @class tokenizer::StructureTokenizer
+     * @brief This class represent an object that retrieves token from a
+     * structure
+     */
+    class StructureTokenizer : public QObject {
+        public:
+            /**
+             * @brief Constructor
+             *
+             * @param parent parent QObject
+             */
+            explicit StructureTokenizer(QObject * parent = 0);
+
+            /**
+             * @brief Divide the structure in tokens
+             *
+             * @param structure the specified structure
+             *
+             * @return the list of tokens contained in the structure
+             * or empty if no token are contained in the structure
+             */
+            TokenList_t tokenize(const QString & structure);
+
+            /**
+             * @brief get the last list of tokens computed by this object
+             *
+             * @return the list of tokens, empty if no list was
+             * previously computed
+             */
+            TokenList_t getLastTokenList() const;
+
+        private:
+            TokenList_t lastTokenList;
+    };
+}
+
+
+#endif // STRUCTURE_TOKENIZER_H
